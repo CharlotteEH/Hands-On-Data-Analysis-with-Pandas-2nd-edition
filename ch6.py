3bfe77cdf05d10e7dd508ff3c93b772795cb053c
Charlotte.Henstock <charlotteehenstock@gmail.com>
2023-04-04 16:59:22 +0100

    04-04-2023

--- ch6.py
+++ ch6.py
@@ -69,3 +69,214 @@ sns.pairplot(
     hue='quarter'
 )
 plt.show()
+
+sns.jointplot(
+    x='log_volume',
+    y='max_abs_change',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low
+    )
+)
+plt.show()
+
+sns.jointplot(
+    x='log_volume',
+    y='max_abs_change',
+    kind='hex',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low
+    )
+)
+plt.show()
+
+sns.jointplot(
+    x='log_volume',
+    y='max_abs_change',
+    kind='reg',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low
+    )
+)
+plt.show()
+
+sns.jointplot(
+    x='log_volume',
+    y='max_abs_change',
+    kind='kde',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low
+    )
+)
+plt.show()
+
+sns.jointplot(
+    x='log_volume',
+    y='max_abs_change',
+    kind='resid',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low
+    )
+)
+# update y-axis label (discussed in the next notebook)
+plt.ylabel('residuals')
+plt.show()
+
+fb_reg_data = fb.assign(
+    log_volume=np.log(fb.volume),
+    max_abs_change=fb.high - fb.low
+).iloc[:,-2:]
+
+import itertools
+
+iterator = itertools.repeat("I'm an iterator", 1)
+
+for i in iterator:
+    print(f'-->{i}')
+print('This printed once because the iterator has been exhausted')
+for i in iterator:
+    print(f'-->{i}')
+
+iterable = list(itertools.repeat("I'm an iterable", 1))
+
+for i in iterable:
+    print(f'-->{i}')
+print('This prints again because it\'s an iterable:')
+for i in iterable:
+    print(f'-->{i}')
+
+from viz import reg_resid_plots
+reg_resid_plots??
+
+reg_resid_plots(fb_reg_data)
+
+sns.lmplot(
+    x='log_volume',
+    y='max_abs_change',
+    data=fb.assign(
+        log_volume=np.log(fb.volume),
+        max_abs_change=fb.high - fb.low,
+        quarter=lambda x: x.index.quarter
+    ),
+    col='quarter'
+)
+
+g=sns.FacetGrid(
+    quakes.query(
+        'parsed_place.isin(["Indonesia", "Papua New Guinea"] )'
+        'and magType == "mb"'
+    ),
+    row='tsunami',
+    col='parsed_place',
+    height=4
+)
+g = g.map(sns.histplot, 'mag', kde=True)
+
+# 2-formatting_plots.ipynb
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+fb = pd.read_csv(
+    'C:/Users/charlotte.henstock/PycharmProjects/pythonProject2/ch_06/data/fb_stock_prices_2018.csv', index_col='date', parse_dates=True
+)
+covid = pd.read_csv('C:/Users/charlotte.henstock/PycharmProjects/pythonProject2/ch_06/data/covid19_cases.csv').assign(
+    date=lambda x: pd.to_datetime(x.dateRep, format='%d/%m/%Y')
+).set_index('date').replace(
+    'United_States_of_America', 'USA'
+).sort_index()['2020-01-18':'2020-09-18']
+
+fb.close.plot()
+plt.title('FB Closing Price')
+plt.xlabel('date')
+plt.ylabel('price ($)')
+plt.show()
+
+fb.iloc[:,:4].plot(subplots=True, layout=(2, 2), figsize=(12, 5))
+plt.title('Facebook 2018 Stock Data')
+plt.ylabel('price ($)')
+plt.show()
+
+axes = fb.iloc[:, :4].plot(subplots=True, layout=(2, 2), figsize=(12, 5))
+plt.suptitle('Facebook 2018 Stock Data')
+for ax in axes.flatten():
+    ax.set_ylabel('price ($)')
+plt.show()
+
+fb.assign(
+    ma=lambda x: x.close.rolling(20).mean()
+).plot(
+    y=['close', 'ma'],
+    title='FB closing price in 2018',#
+    label=['closing price', '20D moving average'],
+    style=['-', '--']
+)
+plt.legend(loc='lower left')
+plt.ylabel('price ($)')
+plt.show()
+
+new_cases = covid.reset_index().pivot(
+    index='date', columns='countriesAndTerritories', values='cases'
+).fillna(0)
+
+pct_new_cases = new_cases.apply(lambda x: x/new_cases.apply('sum', axis=1), axis=0)[
+    ['Italy', 'China', 'Spain', 'USA', 'India', 'Brazil']
+].sort_index(axis=1).fillna(0)
+
+ax = pct_new_cases.plot(
+    figsize=(12, 7), style=['-']*3+['--', ':', '-.'],
+    title='Percentage of the world\'s new covid cases\n(source: ecdc)'
+)
+
+ax.legend(title='Country', framealpha=0.5, ncol=2)
+ax.set_xlabel('')
+ax.set_ylabel('percentage of the world\'s covid cases')
+
+for spine in ['top', 'right']:
+    ax.spines[spine].set_visible(False)
+plt.show()
+
+
+from matplotlib.ticker import PercentFormatter
+
+ax = pct_new_cases.plot(
+    figsize=(12, 7), style=['-']*3+['--', ':', '-.'],
+    title='Percentage of the world\'s new covid cases\n (source: ecdc)'
+)
+
+tick_locs=covid.index[covid.index.day==18].unique()
+tick_labels=[loc.strftime('%b %d\n%Y') for loc in tick_locs]
+plt.xticks(tick_locs, tick_labels)
+
+ax.legend(title='', framealpha=0.5, ncol=2)
+ax.set_xlabel('')
+ax.set_ylabel('percentage of the world\'s covid cases')
+ax.set_ylim(0, None)
+ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
+
+for spine in ['top', 'right']:
+    ax.spines[spine].set_visible(False)
+
+plt.show()
+
+from matplotlib.ticker import EngFormatter
+
+ax = covid.query('continentExp !="Other"').groupby([
+    'continentExp', pd.Grouper(freq='1D')
+]).cases.sum().unstack(0).apply('cumsum').plot(
+    style=['-', '-', '--', ':', '-.'],
+    title='Cumulative covid cases per continent\n(source- ecdc)'
+)
+
+ax.legend(title='',loc='center left')
+ax.set(xlabel='', ylabel='total covid cases')
+ax.yaxis.set_major_formatter(EngFormatter())
+
+for spine in ['top', 'right']:
+    ax.spines[spine].set_visible(False)
+plt.show()

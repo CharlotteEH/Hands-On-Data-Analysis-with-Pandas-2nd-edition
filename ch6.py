ad783da5c2964b6b7687bbbdbfbcdbb7db228865
Charlotte.Henstock <charlotteehenstock@gmail.com>
2023-04-06 16:06:28 +0100

    06-04-2023

--- ch6.py
+++ ch6.py
@@ -507,27 +507,185 @@ year_colors
 
 next(year_colors)
 
+year_colors = (
+    '#ffffff'
+    if (not year % 100 and year % 400) or year % 4
+    else '#000000' for year in range (1992, 200019)
+)
+next(year_colors)
 
 
+weekly_volume_traded = fb.loc['2018-Q4'].groupby(pd.Grouper(freq='W')).volume.sum()
+weekly_volume_traded.index = weekly_volume_traded.index.strftime('W %W')
+ax = weekly_volume_traded.plot(
+    kind='bar', hatch='*', color='pink',
+    title='Volume traded per week in Q4 2018'
+)
+ax.set(xlabel='week number', ylabel='volume traded')
+
+import calendar
+
+db_q4 = fb.loc['2018-Q4']
+
+for texture, month in zip(['oo', '/\\/\\', '...'], [10, 11, 12]):
+    plt.fill_between(
+        fb_q4.index, fb_q4.high, fb_q4.low, hatch=texture,
+        where=fb_q4.index.month == month, facecolor='pink',
+        label=f'{calendar.month_name[month]} differential'
+    )
+
+plt.plot(fb_q4.index, fb_q4.high, '--', label='daily high')
+plt.plot(fb_q4.index, fb_q4.low, '--', label='daily low')
+plt.xticks(['2018-10-01', '2018-11-01', '2018-12-01', '2019-01-01'])
+plt.xlabel('date')
+plt.ylabel('price ($)')
+plt.title('FB differential between high and low price Q4 2018')
+plt.legend()
 
 
+# exercises
+
+# 1. heat map to visualise the correlation coefficients  between earthquake magnitude and whether there
+# was a tsunami for earthquakes with the mb type.
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+fb = pd.read_csv('C:/Users/charlotte.henstock/PycharmProjects/pythonProject2/ch_06/data/fb_stock_prices_2018.csv', index_col='date', parse_dates=True)
+quakes = pd.read_csv('C:/Users/charlotte.henstock/PycharmProjects/pythonProject2/ch_06/data/earthquakes.csv')
+covid = pd.read_csv('C:/Users/charlotte.henstock/PycharmProjects/pythonProject2/ch_06/data/covid19_cases.csv').assign(
+    date=lambda x: pd.to_datetime(x.dateRep, format='%d/%m/%Y')
+).set_index('date').replace(
+    'United_States_of_America', 'USA'
+).sort_index()['2020-01-18':'2020-09-18']
+
+sns.heatmap(
+    quakes.query('magType == "mb"')[['mag','tsunami']].corr(),
+    annot=True, center=0, vmin=-1, vmax=1
+)
+plt.yticks(rotation=0)
+plt.title('Correlation between earthquake magnitude\nand whether there was a tsunami', y=1.01)
+
+lation between earthquake magnitude\nand whether there was a tsunami', y=1.01)
+Text(0.5, 1.01, 'Correl
+
+# 2. boxplot of facebook volume traded and closing prices.  Tukey box plot
+
+cols = ['volume', 'close']
+subset=fb[cols]
+
+quantiles = subset.quantile([0.25, 0.75])
+quantiles.loc['iqr', :]=quantiles.loc[0.75,:] - quantiles.loc[0.25,:]
+
+axes = subset.plot(kind='box', subplots=True, figsize=(8, 3), title=['Volume Traded', 'Closing Price'])
+for ax, col in zip(axes, cols):
+    stats = quantiles[col]
+    lower = stats.loc[0.25] - 1.5*stats['iqr']
+    upper = stats.loc[0.75] + 1.5*stats['iqr']
+    for bound, name in zip ([lower, upper], ['lower', 'upper']):
+        ax.axhline(
+            bound,
+            color='red',
+            linestyle='dashed',
+            label=name
+        )
+        ax.legend()
+plt.suptitle('Facebook volume traded and closing price with Tukey Fence bounds', y=1.1)
+
+# 3.  cumulative covid cases.  dashed line when it reaches 1million
+
+from matplotlib.ticker import EngFormatter
+
+cumulative_cases = covid.groupby(pd.Grouper(freq='1D')).sum().cases.cumsum()
+
+ax = cumulative_cases.plot(title='Cumulative covid-19 cases worldwide\n(source:ECDC)', figsize=(10, 4))
+ax.set_ylabel('cumulative covid cases')
+ax.yaxis.set_major_formatter(EngFormatter())
+
+over_1M = cumulative_cases[cumulative_cases>=1e6].index[0]
+ax.axvline(over_1M, linestyle='dashed', color='k', alpha=0.5, label=f"Reached 1M on {over_1M.strftime('%Y-%m-%d')}")
+
+ax.legend()
 
 
+# 4. avxspan to shade a rectangle 2018-07-25 to 2018-07-31.  large decline of stock price
+
+fb.close.plot(kind='line', figsize=(10, 3))
+plt.axvspan(
+    '2018-07-25', '2018-07-31', alpha=0.2, label='July decline',
+)
+plt.ylabel('Price ($)')
+plt.suptitle('Facebook Closing Price')
+plt.legend()
 
 
+# 5.  annotate facebook stocks with disappointing user growth announced.  cambridge analytica
+# and ftc investigation
+
+fb.close.plot(kind='line', figsize=(5, 3))
+
+notes = [
+    ('Disappointing user growth announced after close', '2018-07-25'),
+    ('Cambridge Analytica story breaks', '2018-03-19'),
+    ('FTC launches investigation', '2018-03-20')
+]
+
+np.random.seed(1)
+
+for text, date in notes:
+    y_value = fb.close[date]
+    jitter = np.random.uniform(-20, -10, 1)
+    plt.annotate(
+        text,
+        xy=(date, y_value),
+        xytext=('2019-02-25', y_value + jitter),
+        arrowprops=dict(arrowstyle='->')
+    )
+plt.ylabel('Price ($)')
+plt.suptitle('Facebook Closing Price')
+
+# 6. modify the reg resid plots to use a matplot lib color map
+import itertools
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
 
 
+def reg_resid_plots(data):
+    """
+    Using `seaborn`, plot the regression and residuals
+    plots side-by-side for every permutation of 2 columns
+    in the data.
 
+    Parameters:
+        - data: A `pandas.DataFrame` object
 
+    Returns:
+        A matplotlib `Axes` object.
+    """
+    num_cols = data.shape[1]
+    permutation_count = num_cols * (num_cols - 1)
 
+    fig, ax = plt.subplots(
+        permutation_count,
+        2,
+        figsize=(15, 4 * permutation_count)
+    )
 
+    for (x, y), axes, color in zip(
+            itertools.permutations(data.columns, 2),
+            ax,
+            [cm.Dark2(i) for i in range(len(ax))]
+    ):
+        for subplot, func in zip(axes, (sns.regplot, sns.residplot)):
+            func(x=x, y=y, data=data, ax=subplot, color=color)
 
+            if func == sns.residplot:
+                # mark the residuals as such
+                subplot.set_ylabel('residuals')
 
+    return fig.axes
 
-
-
-
-
-
-
-
+reg_resid_plots(fb[['close', 'volume']])
